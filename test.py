4fe4adab513b83824a2f92ee233e7fed7b8397bd
daluzi <1210415250@qq.com>
2019-09-26 13:24:59 +0800

    zip

---
+++ test.py
@@ -0,0 +1,31 @@
+# _*_ coding: utf-8 _*_
+# @Author   : daluzi
+# @time     : 2019/9/26 13:12
+# @File     : test.py
+# @Software : PyCharm
+
+list1 = [1, 2, 3, 4]
+tuple1 = zip(list1)
+tuple1 = list(tuple1)
+print(tuple1)#[(1,), (2,), (3,), (4,)]
+
+## zip()函数有2个参数
+m = [[1, 2, 3],  [4, 5, 6],  [7, 8, 9]]
+n = [[2, 2, 2],  [3, 3, 3],  [4, 4, 4]]
+p = [[2, 2, 2],  [3, 3, 3]]
+# 行与列相同
+print("行与列相同:\n", list(zip(m, n)))
+# 行与列不同
+print("行与列不同:\n", list(zip(m, p)))
+
+
+## zip()应用
+# 矩阵相加减、点乘
+m = [[1, 2, 3],  [4, 5, 6],  [7, 8, 9]]
+n = [[2, 2, 2],  [3, 3, 3],  [4, 4, 4]]
+# 矩阵点乘
+print('=*'*10 + "矩阵点乘" + '=*'*10)
+print([x*y for a, b in zip(m, n) for x, y in zip(a, b)])
+# 矩阵相加,相减雷同
+print('=*'*10 + "矩阵相加,相减" + '=*'*10)
+print([x+y for a, b in zip(m, n) for x, y in zip(a, b)])
