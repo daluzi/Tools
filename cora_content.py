cea3cde535827351179079b4b8a7d53d91d9a927
daluzi <1210415250@qq.com>
2019-10-09 19:28:28 +0800

    cora_content dataset to .mat

--- cora_content.py
+++ cora_content.py
@@ -6,23 +6,52 @@
 
 import numpy as  np
 import os
-import scipy.io as scio
-import numpy
+import scipy.io as sio
+import numpy as np
 
 file_path = "./cora/cora.content"
-txt_path = "./cora/cora_content.txt"
+mat_path = "./cora/cora_content.mat"
 
 def ReadTxtData(filePath):
 	resultData = []
 	gnd = []
 	with open(filePath,"r") as f:
 		for line in f:
-			resultData.append(list(line.strip("\n").split("\t")))
-			a = list(line.strip("\n").split("\t"))[1434]
+			f = list(line.strip("\n").split("\t"))
+			del f[0]
+			resultData.append(f)
+			a = f[1433]
 			if a not in gnd:
-				gnd.append(list(line.strip("\n").split("\t"))[1434])
+				gnd.append(a)
 	return resultData,gnd
 
 
 r,g = ReadTxtData(file_path)
+r = np.array(r)
+print("r:\n",r)
 print(g)
+
+dic = {}
+for i in range(len(g)):
+	asd = g[i]
+	dic.update({asd:i+1})
+print(list(dic.keys())[0])
+print(dic)
+
+for i in range(len(r)):
+	# print(r[i][1433])
+	for j in range(7):
+		if r[i][1433] == list(dic.keys())[j]:
+			r[i][1433] = list(dic.values())[j]
+
+print("R\n",r)
+print(type(r))
+print(r.shape)
+r_to_int = []
+for i in range(len(r)):
+	w = r[i]
+	r_to_int.append(list(map(lambda x:int(x), w)))
+r_to_int = np.array(r_to_int)
+sio.savemat(mat_path, {'content':r_to_int})
+data = sio.loadmat(mat_path)
+print(data['content'])
