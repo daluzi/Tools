a2836d49ddd0b581d61cb95b98f16b0cde8168d5
daluzi <1210415250@qq.com>
2019-10-10 17:35:42 +0800

    VSS,PCC

--- metrices.py
+++ metrices.py
@@ -3,3 +3,35 @@
 # @time     : 2019/10/10 15:26
 # @File     : metrices.py
 # @Software : PyCharm
+import numpy as np
+import math
+
+'''
+	VSS
+'''
+def VectorSpaceSimilarity(R,I,si,sf):
+	'''
+	:param R: m*n rating matrix,m users,n items
+	:param I: m*n demension, according to R: if user ui rated item vj,then Iij is equal to 1. and equal to 0 otherwise
+	:param si: user i
+	:param sf: user f
+	:return: VSS
+	'''
+	sumRijRfj = 0
+	sumRij = 0
+	sumRfj = 0
+	Rows,Columns = np.shape(R)
+	for i in range(Columns):
+		if I[si][i] == 1 and I[sf][i] == 1:
+			sumRijRfj +=  R[si][i] * R[sf][i]
+			sumRij += np.square(R[si][i])
+			sumRfj += np.square(R[sf][i])
+	simIF = sumRijRfj / (np.sqrt(sumRij) * np.sqrt(sumRfj))
+	return simIF
+
+'''
+	PCC
+'''
+def PearsonCorrelationCoefficient():
+	
+	pass
