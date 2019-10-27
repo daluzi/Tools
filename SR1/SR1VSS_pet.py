05f2ad1866373e686adbe77771d0f403317c736c
daluzi <1210415250@qq.com>
2019-10-27 11:30:04 +0800

    SR1 VSS

--- SR1/SR1VSS_pet.py
+++ SR1/SR1VSS_pet.py
@@ -117,7 +117,7 @@ class SR1:
 	def __init__(self,filepath,k):
 		readData = ReadTxtData(filepath)#读取文件'
 		r, train, test = ProData(readData)
-		U, V = self.Update(train, k, 10, 0.001, 0.001, 0.001, 0.01)
+		U, V = self.Update(train, k, 10, 0.001, 0.001, 0.001, 0.02)
 		print("----------------------------------------------")
 		print("U:\n",U)
 		print("V:\n",V)
@@ -220,7 +220,7 @@ class SR1:
 
 if __name__ == '__main__':
 	filePath = './pets/ratings.txt'
-	k = 200
+	k = 300
 	sr1 = SR1(filePath, k)
 	# newX = [[sr1.new[i][j] + sr1.r for j in range(len(sr1.new[i]))] for i in range(len(sr1.new))]  # 每个元素累加r
 	newX = sr1.new
