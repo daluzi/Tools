1e232568ad2fd0f45f7b12bd22a5404846e0aef2
daluzi <1210415250@qq.com>
2020-07-14 12:25:57 +0800

    yield

---
+++ yield_test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2020/7/14 10:53
+@Author  : daluzi
+@File    : yield_test.py
+'''
+
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+g = foo()
+print(next(g))
+print("*"*20)
+print(next(g))
+
+'''
+starting...
+4
+********************
+res: None
+4
+'''
+'''
+1.程序开始执行以后，因为foo函数中有yield关键字，所以foo函数并不会真的执行，而是先得到一个生成器g(相当于一个对象)
+
+2.直到调用next方法，foo函数正式开始执行，先执行foo函数中的print方法，然后进入while循环
+
+3.程序遇到yield关键字，然后把yield想想成return,return了一个4之后，程序停止，并没有执行赋值给res操作，此时next(g)语句执行完成，所以输出的前两行（第一个是while上面的print的结果,第二个是return出的结果）是执行print(next(g))的结果，
+
+4.程序执行print("*"*20)，输出20个*
+
+5.又开始执行下面的print(next(g)),这个时候和上面那个差不多，不过不同的是，这个时候是从刚才那个next程序停止的地方开始执行的，也就是要执行res的赋值操作，这时候要注意，这个时候赋值操作的右边是没有值的（因为刚才那个是return出去了，并没有给赋值操作的左边传参数），所以这个时候res赋值是None,所以接着下面的输出就是res:None,
+
+6.程序会继续在while里执行，又一次碰到yield,这个时候同样return 出4，然后程序停止，print函数输出的4就是这次return出的4.
+'''
+
+
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+g = foo()
+print(next(g))
+print("*"*20)
+print(g.send(7))
+'''
+starting...
+4
+********************
+res: 7
+4
+'''
+'''
+先大致说一下send函数的概念：此时你应该注意到上面那个的紫色的字，还有上面那个res的值为什么是None，这个变成了7，到底为什么，这是因为，send是发送一个参数给res的，因为上面讲到，return的时候，并没有把4赋值给res，下次执行的时候只好继续执行赋值操作，只好赋值为None了，而如果用send的话，开始执行的时候，先接着上一次（return 4之后）执行，先把7赋值给了res,然后执行next的作用，遇见下一回的yield，return出结果后结束。
+
+'''
