4663d0cc277659efcdbff9c0119a032632b1a9e8
daluzi <1210415250@qq.com>
2020-12-04 22:27:05 +0800

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2020/12/4 22:23
+@Author  : daluzi
+@File    : test.py
+'''
+
+
+num = 1
+print("%d" % (num))
+print("%2d" % (num))
+print("%02d" % (num))
+print("%-2d" % (num))
+print("%.2d" % (num))
